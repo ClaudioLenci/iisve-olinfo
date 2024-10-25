9abe58814feaaeb63c35f36b50109dec9091b6a7
Claudio Lenci <lenciclaudio06@gmail.com>
2024-10-25 21:42:36 +0200

    Update logo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,13 +1,13 @@
-import olinfoImg from "@/assets/olinfo.png";
+import olinfoImg from "@/assets/olinfo.svg";
 import mascotteImg from "@/assets/mascotte.png";
 
 export function Logo() {
   return (
-    <div className="flex justify-center gap-x-32 h-80">
-      <div>
-        <img src={olinfoImg} className="h-full" />
+    <div className="flex justify-center gap-x-32 h-80 w-[50vw] m-auto">
+      <div className="flex justify-center basis-1/2 bg-[radial-gradient(circle,_rgba(51,51,51,1)_20%,_rgba(38,38,38,1)_35%)]">
+        <img src={olinfoImg} className="h-full py-5" />
       </div>
-      <div>
+      <div className="flex justify-center basis-1/2 bg-[radial-gradient(circle,_rgba(51,51,51,1)_20%,_rgba(38,38,38,1)_35%)]">
         <img src={mascotteImg} className="h-full" />
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,9 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'light-black': '#262626'
+        'light-black': '#262626',
+        'dark-black': '#1a1a1a',
+        'light2-black': '#333333',
       },
       fontFamily: {
         'primary': ['ui-sans-serif', 'system-ui', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji']
