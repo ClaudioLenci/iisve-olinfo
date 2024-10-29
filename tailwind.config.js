b7b15d8ebabb5f806350505875dd3a1f7b53934f
ClaudioLenci <73424774+ClaudioLenci@users.noreply.github.com>
2024-10-29 17:05:21 +0100

    Add @heroicons/react package + add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,10 @@ module.exports = {
         'light-black': '#262626',
         'dark-black': '#1a1a1a',
         'light2-black': '#333333',
-        'text-white': '#a6adbb'
+        'text-white': '#a6adbb',
+        'text-white2': '#dadada',
+        'gray1': '#3b444b',
+        'gray2': '#414a4c'
       },
       fontFamily: {
         'primary': ['ui-sans-serif', 'system-ui', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji']
